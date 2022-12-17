a72b417da4315e6ae8fa0e8e26207861dc11c8be
Polushin Dima <dima2004polushin2004@gmail.com>
2022-12-17 19:53:24 +0600

    Создал компонент 'задача' и сделал верстку

---
+++ src/components/TaskItem/TaskItem.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Box, Checkbox, IconButton, Paper, Typography} from "@mui/material";
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const TaskItem = () => {
+  return (
+    <Paper
+      elevation={3}
+      sx={{py: 3, pl: 1, mb: 3,
+        backgroundColor: '#ff6767',
+        borderRadius: 4,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <Typography component='h3' variant='h5' sx={{color: '#fff' }} ></Typography>
+      <Box>
+        <IconButton aria-label="delete">
+          <DeleteIcon sx={{fontSize: 28, color: '#fff'}} />
+        </IconButton>
+        <Checkbox
+          sx={{ '& .MuiSvgIcon-root': { fontSize: 28 }, color: '#fff' }}
+          icon={<FavoriteBorder />}
+          checkedIcon={<Favorite sx={{color: '#fff'}}/>}
+        />
+      </Box>
+    </Paper>
+  );
+};
+
+export default TaskItem;

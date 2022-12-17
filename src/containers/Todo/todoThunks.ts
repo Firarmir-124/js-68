ac93d99c0bd36baff8c0bef5c46ff10ab1c866ba
Polushin Dima <dima2004polushin2004@gmail.com>
2022-12-17 20:33:53 +0600

    Создал функцию получение из сервера 'задачи'

--- src/containers/Todo/todoThunks.ts
+++ src/containers/Todo/todoThunks.ts
@@ -8,3 +8,23 @@ export const addTask = createAsyncThunk<void, TaskMutation>(
     await axiosApi.post('/todo.json', arg);
    }
 )
+
+export const fetchTasks = createAsyncThunk(
+  'task/fetch',
+  async () => {
+    const resTasks = await axiosApi.get('/todo.json');
+    const tasks = resTasks.data;
+    let newTasks:Task[] = [];
+
+    if (tasks) {
+      newTasks = Object.keys(tasks).map((id) => {
+        const task = tasks[id];
+        return {
+          ...task,
+          id,
+        };
+      });
+    }
+    return newTasks;
+  }
+);

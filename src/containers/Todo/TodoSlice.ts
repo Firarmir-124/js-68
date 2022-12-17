32277f2270141de3b86c929244d6b4422c3b6c1f
Polushin Dima <dima2004polushin2004@gmail.com>
2022-12-17 20:17:16 +0600

    Обновил компонент slice

--- src/containers/Todo/TodoSlice.ts
+++ src/containers/Todo/TodoSlice.ts
@@ -1,5 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
 import {Task} from "../../types";
+import {addTask} from "./todoThunks";
 
 interface TaskState {
   loaderBtn: boolean;
@@ -15,7 +16,17 @@ const todoSlice = createSlice({
   name: 'todo',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {}
+  extraReducers: (builder) => {
+    builder.addCase(addTask.pending, (state) => {
+      state.loaderBtn = true;
+    });
+    builder.addCase(addTask.fulfilled, (state) => {
+      state.loaderBtn = false;
+    });
+    builder.addCase(addTask.rejected, (state) => {
+      state.loaderBtn = false;
+    })
+  }
 })
 
 export const todoReducer = todoSlice.reducer;
